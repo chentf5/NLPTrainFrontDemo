a14c1b3d8fc9d7b413469379787817288b199682
chentongfei <chentongfei@bytedance.com>
2020-06-09 22:20:15 +0800

    all

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
-
+  model: any;
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.model =  await loadGraphModel('web_model/model.json');
   }
+
+
 
 }
